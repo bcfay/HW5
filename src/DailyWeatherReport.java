8084b6a832360236f466e901bebade97fe79e3e1
brian <briancfay1@gmail.com>
2019-12-01 19:58:09 -0500

    implemented all functions

--- src/DailyWeatherReport.java
+++ src/DailyWeatherReport.java
@@ -2,14 +2,42 @@ import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
 public class DailyWeatherReport {
-    GregorianCalendar date;
-    LinkedList temperature;
-    LinkedList rainfall;
+    private GregorianCalendar date;
+    private LinkedList temperature;
+    private LinkedList rainfall;
 
 
     public DailyWeatherReport(GregorianCalendar date, LinkedList temperature, LinkedList rainfall) {
         this.date = date;
         this.temperature = temperature;
         this.rainfall = rainfall;
+    }
+
+    public GregorianCalendar getDate() {
+        return date;
+    }
+
+    public LinkedList getTemperatureList() {
+        return temperature;
+    }
+
+    public LinkedList getRainfallList() {
+        return rainfall;
+    }
+
+    public double getTemperature(){
+        return sumList(this.temperature);
+    }
+
+    public double getRainfall(){
+        return sumList(this.rainfall);
+    }
+
+    private double sumList(LinkedList<Double> readings){
+        double sum=0;
+        for(double i :readings){
+            sum+=i;
+        }
+        return sum;
     }
 }
--- src/WeatherMonitor.java
+++ src/WeatherMonitor.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 
 public class WeatherMonitor {
 
-    LinkedList reports;
+    LinkedList<DailyWeatherReport> reports;
 
     public WeatherMonitor(LinkedList reports) {
         this.reports = reports;
@@ -13,17 +13,48 @@ public class WeatherMonitor {
         this.reports = new LinkedList();
     }
 
-    public float averageTempForMonth(int month, int year){
-
-        return 0;
+    public double averageTempForMonth(int month, int year){
+        double totalTemp=0;
+        int i=0;
+        for(DailyWeatherReport m: reportsForMonth(month, year)){
+            totalTemp += m.getTemperature();
+            i++;
+        }
+        return totalTemp/i;
     }
 
-    public float totalRainfallForMonth(int month){
-        return 0;
+    public double totalRainfallForMonth(int month, int year){
+        double totalRainfall=0;
+        for(DailyWeatherReport m: reportsForMonth(month, year)){
+            totalRainfall += m.getRainfall();
+        }
+        return totalRainfall;
     }
 
-    public void addDailyReport(GregorianCalendar date, LinkedList readings){
+    public void addDailyReport(GregorianCalendar date, LinkedList<Reading> readings){
+            reports.add(createReport(date,readings));
+    }
 
+    private DailyWeatherReport createReport(GregorianCalendar date, LinkedList<Reading> readings){
+        LinkedList tempurature= new LinkedList();
+        LinkedList rainfall = new LinkedList();
+        for(Reading r : readings){
+            tempurature.add(r.getTempurature());
+            rainfall.add(r.getRainfall());
+        }
+
+        return new DailyWeatherReport(date, tempurature, rainfall);
+    }
+
+    private LinkedList<DailyWeatherReport> reportsForMonth(int month, int year){
+        LinkedList<DailyWeatherReport> monthsReports = new LinkedList<>();
+
+        for(DailyWeatherReport d: this.reports){
+            if((d.getDate().get((GregorianCalendar.MONTH)) == month) && (d.getDate().get((GregorianCalendar.YEAR)) == year)){
+                monthsReports.add(d);
+            }
+        }
+        return monthsReports;
     }
 
 
